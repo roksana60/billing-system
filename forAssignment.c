49c8f53f6eb64ada8a05360f2a84dc9891853bc6
roksana60 <105806394+roksana60@users.noreply.github.com>
2022-05-24 00:38:22 +0600

    Update forAssignment.c

--- forAssignment.c
+++ forAssignment.c
@@ -5,20 +5,22 @@ typedef struct node
 {
     struct node *next;
     int vertex;
-}node;
+}
+node;
 node *G[20];  
+//heads of linked list
 int visited[20];
 int n;
 void read_graph();
-
+//creating adjacency list
 void insert(int,int);  
-
+//insert an edge in adjacency list
 void DFS(int);
 void main()
 {
     int i;
     read_graph();
- 
+    //starting with 0
   
 for(i=0;i<n;i++)
         visited[i]=0;
@@ -46,11 +48,12 @@ void read_graph()
     printf("Enter number of vertices:");
   
 scanf("%d",&n);
-  
+    //starting G[] with NULL
+
 for(i=0;i<n;i++)
     {
         G[i]=NULL;
-
+        //taking input of Edges and inserting them in G[]
       
 printf("Enter number of edges:");
        scanf("%d",&no_of_edges);
@@ -64,17 +67,17 @@ insert(vi,vj);
 }
 void insert(int vi,int vj)
 {
-    node *p,*q;
-    
-
+    node *p,*q; 
+//creating room at the memory with malloc
 q=(node*)malloc(sizeof(node));
     q->vertex=vj;
     q->next=NULL;
-
+    //inserting vi in the linked list
     if(G[vi]==NULL)
         G[vi]=q;
     else
     {
+        //pointing the end of the linked list
         p=G[vi];
       
 while(p->next!=NULL)
